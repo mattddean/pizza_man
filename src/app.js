e1a89a8f061dca11ffff65f9944715a7649db431
tshupp <shupp.tyrin@gmail.com>
2019-12-05 00:45:20 -0500

    Basic ToppingsIntents Created

--- src/app.js
+++ src/app.js
@@ -13,10 +13,10 @@ const { FileDb } = require('jovo-db-filedb');
 const app = new App();
 
 app.use(
-    new Alexa(),
-    new GoogleAssistant(),
-    new JovoDebugger(),
-    new FileDb()
+  new Alexa(),
+  new GoogleAssistant(),
+  new JovoDebugger(),
+  new FileDb()
 );
 
 
@@ -31,49 +31,52 @@ app.setHandler({
 
   HelloWorldIntent() {
     this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
+    this.followUpState('MyNameIsIntent');
   },
 
   MyNameIsIntent() {
     this.tell('Hey ' + this.$inputs.name.value + ', nice to meet you!');
+    return this.toIntent('AskToppingsIntent');
   },
 
   AskToppingsIntent() {
     this.ask('What toppings would you like on your pizza?');
-    return this.toIntent('TellToppingsIntent');
+    // return this.toIntent('TellToppingsIntent');
   },
 
   TellToppingsIntent() {
+    // TODO Change join to place commas for 3+ items
     this.tell('Okay, I\'ll add ' + this.$inputs.toppings.value.join(' and ') + ' to your pizza');
-    return this.toIntent('AskCrustIntent');
-  },
-
-  AskCrustIntent() {
-    this.ask('What type of crust do you want?');
-    return this.toIntent('TellCrustIntent');
-  },
-
-  TellCrustIntent() {
-    this.tell('Okay, your pizza will have ' + this.$inputs.crust.value + 'crust');
-    return this.toIntent('AskSidesIntent');
-  },
-
-  AskSidesIntent() {
-    this.ask('What sides would you like with your pizza?');
-    return this.toIntent('TellSidesIntent');
-  },
-
-  TellSidesIntent() {
-    this.tell('Okay, I\'ll add ' + this.$inputs.sides.value.join(' and ') + 'to your order');
-    return this.toIntent('ConfirmOrderIntent');
-  },
-
-  CancelOrderIntent() {
-    this.tell('Your order is cancelled. Let me know if you need me again!');
-  },
-
-  ConfirmOrderIntent() {
-    this.tell('Your order is confirmed');
+    //   return this.toIntent('AskCrustIntent');
   }
+
+  // AskCrustIntent() {
+  //   this.ask('What type of crust do you want?');
+  //   return this.toIntent('TellCrustIntent');
+  // },
+
+  // TellCrustIntent() {
+  //   this.tell('Okay, your pizza will have ' + this.$inputs.crust.value + 'crust');
+  //   return this.toIntent('AskSidesIntent');
+  // },
+
+  // AskSidesIntent() {
+  //   this.ask('What sides would you like with your pizza?');
+  //   return this.toIntent('TellSidesIntent');
+  // },
+
+  // TellSidesIntent() {
+  //   this.tell('Okay, I\'ll add ' + this.$inputs.sides.value.join(' and ') + 'to your order');
+  //   return this.toIntent('ConfirmOrderIntent');
+  // },
+
+  // CancelOrderIntent() {
+  //   this.tell('Your order is cancelled. Let me know if you need me again!');
+  // },
+
+  // ConfirmOrderIntent() {
+  //   this.tell('Your order is confirmed');
+  // }
 });
 
 module.exports.app = app;
