33debf599dd0fce547659412fc01f1e4d82525fe
Matt Dean <mdean400@gmail.com>
2019-12-05 00:00:53 -0500

    Add initial intents

--- src/app.js
+++ src/app.js
@@ -25,17 +25,55 @@ app.use(
 // ------------------------------------------------------------------
 
 app.setHandler({
-    LAUNCH() {
-        return this.toIntent('HelloWorldIntent');
-    },
+  LAUNCH() {
+    return this.toIntent('HelloWorldIntent');
+  },
 
-    HelloWorldIntent() {
-        this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
-    },
+  HelloWorldIntent() {
+    this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
+  },
 
-    MyNameIsIntent() {
-        this.tell('Hey ' + this.$inputs.name.value + ', nice to meet you!');
-    },
+  MyNameIsIntent() {
+    this.tell('Hey ' + this.$inputs.name.value + ', nice to meet you!');
+  },
+
+  AskToppingsIntent() {
+    this.ask('What toppings would you like on your pizza?');
+    return this.toIntent('TellToppingsIntent');
+  },
+
+  TellToppingsIntent() {
+    this.tell('Okay, I\'ll add ' + this.$inputs.toppings.value.join(' and ') + ' to your pizza');
+    return this.toIntent('AskCrustIntent');
+  },
+
+  AskCrustIntent() {
+    this.ask('What type of crust do you want?');
+    return this.toIntent('TellCrustIntent');
+  }
+
+  TellCrustIntent() {
+    this.tell('Okay, your pizza will have ' + this.$inputs.crust.value + 'crust');
+    return this.toIntent('AskSidesIntent');
+  },
+
+  AskSidesIntent() {
+    this.ask('What sides would you like with your pizza?');
+    return this.toIntent('TellSidesIntent');
+  },
+
+  TellSidesIntent() {
+    this.tell('Okay, I\'ll add ' + this.$inputs.sides.value.join(' and ') + 'to your order');
+    return this.toIntent('ConfirmOrderIntent');
+  },
+
+  CancelOrderIntent() {
+    this.tell('Your order is cancelled. Let me know if you need me again!');
+  },
+
+  ConfirmOrderIntent() {
+    this.tell('Your order is confirmed');
+  }
 });
 
 module.exports.app = app;
